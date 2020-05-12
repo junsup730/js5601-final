78add1d8080c7f02ed89dcf8076ca928a6a27278
Junsup Shin <junsup0730@gmail.com>
2020-05-12 02:56:42 -0400

    First commit

---
+++ js5601-final/app.py
@@ -0,0 +1,17 @@
+
+# import statements
+from flask import Flask, render_template
+
+# Flask app variable
+app = Flask(__name__)
+
+
+# static route
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+
+# start the server
+if __name__ == "__main__":
+    app.run()
